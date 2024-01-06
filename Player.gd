878de474183afcc7e386cfcce073b20dd12ddf48
Daniyar <m.daniyar.abd@gmail.com>
2024-01-07 05:22:00 +0600

    tardis controls graybox

--- Player.gd
+++ Player.gd
@@ -1,14 +1,14 @@
 extends RigidBody3D
-
-var mouse_sensitivity := 0.001
+"""var mouse_sensitivity := 0.001
 var twist_input := 0.0
-var pitch_input := 0.0
+var pitch_input := 0.0"""
 
-var constant_speed := 1200.0
-var velocity := 0.0
+var constant_speed := 6000.0
+var _velocity := 0.0
+var initVelocity 
 
-@onready var twist_pivot := $TwistPivot
-@onready var pitch_pivot := $TwistPivot/PitchPivot
+"""@onready var twist_pivot := $TwistPivot
+@onready var pitch_pivot := $TwistPivot/PitchPivot"""
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -20,20 +20,35 @@ func _process(delta: float) -> void:
 	input.z = Input.get_axis("move_forward", "move_back")
 	
 	if input.x != 0 && input.z != 0:
-		velocity = constant_speed / sqrt(2)
+		_velocity = constant_speed / sqrt(2)
 	else: 
-		velocity = constant_speed
-	apply_central_force(twist_pivot.basis * input * velocity * delta)
+		_velocity = constant_speed
+	#twist_pivot.basis *
+	apply_central_force( input * _velocity * delta)
 	
+	initVelocity = self.linear_velocity 
+	
+	if input.x != 0 || input.z != 0:
+		$MeshInstance3D.rotation.y = lerp_angle($MeshInstance3D.rotation.y, atan2(initVelocity.x, initVelocity.z), delta * 12)
+		
+		$MeshInstance3D.rotation.x = lerp_angle($MeshInstance3D.rotation.x, deg_to_rad(
+			sqrt(
+				pow(initVelocity.x, 2) +
+				pow(initVelocity.y, 2) + 
+				pow(initVelocity.z, 2)
+				)
+			), 
+		delta * 36)
+			
+		$MeshInstance3D.rotation.x = clamp($MeshInstance3D.rotation.x, deg_to_rad(0), deg_to_rad(90))
+	else:
+		$MeshInstance3D.rotation.x = lerp_angle($MeshInstance3D.rotation.x, 0, delta * 2)
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		
-	twist_pivot.rotate_y(twist_input)
+	"""twist_pivot.rotate_y(twist_input)
 	pitch_pivot.rotate_x(pitch_input)
-	pitch_pivot.rotation.x = clamp(pitch_pivot.rotation.x, 
-		deg_to_rad(-30), 
-		deg_to_rad(30)
-	)
+	pitch_pivot.rotation.x = clamp(pitch_pivot.rotation.x, deg_to_rad(-90), deg_to_rad(-90))
 	twist_input = 0.0
 	pitch_input = 0.0
 
@@ -42,4 +57,4 @@ func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 			twist_input = - event.relative.x * mouse_sensitivity
-			pitch_input = - event.relative.y * mouse_sensitivity
+			pitch_input = - event.relative.y * mouse_sensitivity"""
