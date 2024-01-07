072841da7b9df7ba7bac6349cf0103e492c7452e
Daniyar <m.daniyar.abd@gmail.com>
2024-01-07 10:50:56 +0600

    script adjustments, FBX reading plugin, imported tardis model

--- Player.gd
+++ Player.gd
@@ -4,8 +4,6 @@ var constant_speed := 6000.0
 var _velocity := 0.0
 var vLinear #linear velicoty (Vector3)
 
-#@onready var $Mesh.rotation = $Mesh.rotation
-
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	
@@ -25,18 +23,14 @@ func _process(delta: float) -> void:
 	
 	if input.x != 0 || input.z != 0:
 		$Mesh.rotation.y = lerp_angle($Mesh.rotation.y, atan2(vLinear.x, vLinear.z), delta * 12)
-		$Mesh.rotation.x = lerp_angle($Mesh.rotation.x, 
-			deg_to_rad(sqrt(
-				pow(vLinear.x, 2) +
-				pow(vLinear.y, 2) + 
-				pow(vLinear.z, 2)
-				)
-			), delta * 36)
-		$Mesh.rotation.x = clamp($Mesh.rotation.x, deg_to_rad(0), deg_to_rad(90))
-		print("tilting")
-	else:
-		$Mesh.rotation.x = lerp_angle($Mesh.rotation.x, 0, delta * 2)
-		print("standing")
+	$Mesh.rotation.x = lerp_angle($Mesh.rotation.x, 
+		deg_to_rad(sqrt(
+			pow(vLinear.x, 2) +
+			pow(vLinear.y, 2) + 
+			pow(vLinear.z, 2)
+			)
+		), delta * 36)
+	$Mesh.rotation.x = clamp($Mesh.rotation.x, deg_to_rad(0), deg_to_rad(90))
 		
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
